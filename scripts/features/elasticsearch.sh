704b6ac19178416c3984df588a8307e20f3cf49b
tandexiang <tandexiang@shein.com>
2020-03-27 16:38:40 +0800

    Update elasticsearch.sh

--- scripts/features/elasticsearch.sh
+++ scripts/features/elasticsearch.sh
@@ -24,7 +24,8 @@ fi
 
 # Install Java & Elasticsearch
 
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+wget -q -O - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/${version[0]}.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-${version[0]}.x.list
 sudo apt-get update
 sudo apt-get -y install openjdk-11-jre
